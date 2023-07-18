a3076ca2fea3b4e62e61280561676b2d0a07de0e
zelo2 <55820816+zelo2@users.noreply.github.com>
2023-07-18 10:54:17 +0800

    Add files via upload

---
+++ ecargo_summer_schoole_lesson3.py
@@ -0,0 +1,86 @@
+
+import docplex.mp.model as cpx
+import numpy as np
+import time
+
+'''
+Start on 2023/7/18
+End on 2023/7/18
+'''
+
+
+def cplex_demo(stu_num, group_size):
+    '''
+    :param hotel_num: 酒店数量
+    :param restaurant_num: 餐厅数量
+    :param attraction_num: 景点数量
+    :param task_num: 任务数量
+    :return: 休憩点分配矩阵
+    '''
+
+    student_num = stu_num
+    task_num = int(stu_num / group_size)
+
+    row = range(stu_num)
+    column = range(task_num)
+
+    q = np.random.uniform(0, 1, size=[student_num, task_num])
+    c = np.random.binomial(n=1, p=0.1, size=[student_num, task_num])
+
+    p = np.random.binomial(n=1, p=0.7, size=[student_num, task_num])
+    dislike_mark = np.random.binomial(n=1, p=0.1, size=[student_num, task_num])
+    p[dislike_mark == 1] = -1
+
+
+
+    # Model initialization
+    gra_model = cpx.Model(name="GRA lesson 2")
+
+    t = {(i, j): gra_model.binary_var(name="x_{0}_{1}".format(i, j))
+              for i in row for j in column}
+
+    # Constraints
+    for i in row:
+        gra_model.add_constraint(gra_model.sum(t[i, j] for j in column) <= 1)  # one student one group
+
+    for j in column:
+        gra_model.add_constraint(gra_model.sum(t[i, j] for i in row) == group_size)  # group size constraint
+
+    for i in row:
+        for j in column:
+            gra_model.add_constraint(gra_model.sum(t[i, j] * c[i][j] for i in row for j in column) == 0)  # conflict constraint
+
+    # Define objective function
+    objective = gra_model.sum(q[i][j] * t[i, j] * p[i][j] for i in row for j in column)
+    gra_model.maximize(objective)
+
+    # Problem Solving
+    t0 = time.time()
+    result_rsam = gra_model.solve()
+    execution_time = time.time() - t0
+
+    allocation_matrix = np.zeros([student_num, task_num])
+    for i in row:
+        for j in column:
+            if t[i, j].solution_value == 1:
+                allocation_matrix[i - 1, j - 1] = 1
+
+    print("Cplex Allocation Results:")
+    print(allocation_matrix)
+
+    np.savetxt("q.txt", q, fmt='%.3f')
+    np.savetxt("c.txt", c, fmt='%.0f')
+    np.savetxt("p.txt", p, fmt='%.0f')
+    np.savetxt("t.txt", allocation_matrix, fmt='%.0f')
+    return gra_model.objective_value, execution_time, allocation_matrix
+
+
+
+if __name__ == '__main__':
+    # travel day = task_num / 2  旅行天数=任务数/2
+    result = []
+    print(cplex_demo(stu_num=15, group_size=3))
+
+
+
+
